7f37ff1e247386f0955c0da83654bdfa91f7cb54
Patrick Williamson <upm8pb@virginia.edu>
2023-11-09 03:42:25 -0500

    seeing if this works

---
+++ NHL/odds_cleanup.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+season = "2007-08"
+
+url = "https://www.sportsbookreviewsonline.com"
+response = requests.get(url)
+print(response.status_code)
+html = response.text
+soup = BeautifulSoup(html, "lxml")
+
+test = soup.find("div")
+print(test)
+table = soup.find("table")
+print(table)
+if table:
+    rows = table.find("tr")
+    for row in rows:
+            columns = row.find_all("td")
+            for column in columns:
+                print(column.text)
