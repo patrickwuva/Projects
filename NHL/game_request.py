d8c4fac9909ef0751dee8eeec65f88c6c3ef0cbc
Patrick Williamson <upm8pb@virginia.edu>
2023-11-09 22:21:01 -0500

    saving work

--- NHL/game_request.py
+++ NHL/game_request.py
@@ -1,16 +1,19 @@
 import requests
 import json
+import sys
 
 
 def get(year,game):
     
     game = str(game)
 
-    while len(game) < 4:
+    for i in range(len(game),4):
         game = "0"+game
-    print(game)
+
+
     data = []
-    response = requests.get(f"https://api-web.nhle.com/v1/gamecenter/{str(year)}02{game}")
+    response = requests.get(f"https://api-web.nhle.com/v1/gamecenter/{str(year)}02{game}/boxscore")
+    
     if response.status_code != 200:
         data = 404
 
--- NHL/tmp.py
+++ NHL/tmp.py
@@ -3,11 +3,17 @@ import requests
 import sys
 import json
 
-game = 1
-year = 2023
-data = 0
-while data != 404:
+def get_season(data, year, game):
+    print(f"current game: {str(game)}")
     data = game_request.get(year,game)
-    game += 1
-    print(game)
+
+    if data == 404:
+        return game, "done"
     
+    
+    else:
+        print(f"Finishing game: {str(year)} {data['homeTeam']['abbrev']} played {data['awayTeam']['abbrev']} with a final score of {data['boxscore']['linescore']['totals']['home']} - {data['boxscore']['linescore']['totals']['away']}")
+        return get_season(data,year,game+1)
+
+
+get_season(0,2023,1)
