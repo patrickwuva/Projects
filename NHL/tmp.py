bebdad1bdb64c8242838f8fb12f51f011bae3c37
Patrick Williamson <upm8pb@virginia.edu>
2023-11-11 02:56:53 -0500

    lots of changes

--- NHL/tmp.py
+++ NHL/tmp.py
@@ -17,22 +17,27 @@ def get_season(year, game):
         
         data = game_request.get(year,game)
 
-        if data == 404 or data["gameState"] == "FUT":
+        if data == 404 or data["clock"]["timeRemaining"] == "20:00":
             break
+
         print(f"current game: {str(game)}")
 
         season_data.append(data)
 
         game = game + 1
-
-        print( data["gameState"])
       
     return season_data
 
 
-get_season(sys.argv[1],400)
+get_season(sys.argv[1],int(sys.argv[2]))
+
+if season_data:
+
+    with open("data/seasons/2023.json", "w") as file:
+
+        file.write(json.dumps(season_data))
 
 
-with open("data/seasons/2023.json", "w") as file:
 
-    file.write(season_data)
+else:
+    print("No data")
